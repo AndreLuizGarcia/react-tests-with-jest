4b60e3a4bb983a46895a296872bc50b79358d7e2
Andre Garcia <Andre.Garcia@la.logicalis.com>
2019-07-12 15:54:11 -0300

    Redux Reducers component

---
+++ src/store/reducers.js
@@ -0,0 +1,9 @@
+const initialState = [
+  { id: 0, text: 'Fazer café'},
+  { id: 1, text: 'Acessar o slack'},
+  { id: 2, text: 'Ver RocketLive'},
+]
+
+export default function todos(state = initialState, action) {
+  return state
+}
